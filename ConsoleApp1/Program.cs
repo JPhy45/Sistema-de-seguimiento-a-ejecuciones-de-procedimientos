7c418f65f7d5f33371af4ee9b5896389f5034860
JPhy45 <pernia795@gmail.com>
2024-09-04 22:09:22 -0400

    menu sin llenar

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,79 +6,429 @@ using Grpc.Net.Client;
 using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations.Schema;
 using ConsoleApp1;
+using System;
 
-internal class Program
+class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Presione una tecla para conectar");
-        Console.ReadKey();
-
-        Console.WriteLine("Creating channel and client");
-        var httpHandler = new HttpClientHandler();
-        httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
-        var channel = GrpcChannel.ForAddress("http://localhost:5051", new GrpcChannelOptions { HttpHandler = httpHandler });
-        if (channel is null)
+        bool salir = false;
+        while (!salir)
         {
-            Console.WriteLine("Cannot connect");
-            channel.Dispose();
-            return;
+            Console.Clear();
+            Console.WriteLine("Menú Principal");
+            Console.WriteLine("----------------");
+            Console.WriteLine("1. Trabajar con una fase");
+            Console.WriteLine("2. Trabajar con una operación");
+            Console.WriteLine("3. Trabajar con una unidad de procedimiento");
+            Console.WriteLine("4. Trabajar con una ejecución de una fase");
+            Console.WriteLine("5. Trabajar con una ejecución de una operación");
+            Console.WriteLine("6. Trabajar con una ejecución de una unidad de procedimiento");
+            Console.WriteLine("7. Regresar al menú principal");
+
+            Console.Write("Seleccione una opción: ");
+
+            string input = Console.ReadLine();
+            int opcion = -1;
+            if (int.TryParse(input, out opcion))
+            {
+                switch (opcion)
+                {
+                    case 1:
+                        SubMenuFase();
+                        break;
+                    case 2:
+                        SubMenuOperacion();
+                        break;
+                    case 3:
+                        SubMenuUnidadProcedimiento();
+                        break;
+                    case 4:
+                        SubMenuEjecucionFase();
+                        break;
+                    case 5:
+                        SubMenuEjecucionOperacion();
+                        break;
+                    case 6:
+                        SubMenuEjecucionUnidadProcedimiento();
+                        break;
+                    case 7:
+                        continue;
+                    default:
+                        Console.WriteLine("Opción inválida. Por favor, seleccione una opción válida.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Por favor, ingrese un número entero.");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Presione cualquier tecla para continuar...");
+            Console.ReadKey();
         }
+    }
 
-        var client = new gProtos.Phase.PhaseClient(channel);
+    static void SubMenuFase()
+    {
+        bool salirSubMenu = false;
+        while (!salirSubMenu)
+        {
+            Console.Clear();
+            Console.WriteLine("Menú Fase");
+            Console.WriteLine("---------");
+            Console.WriteLine("1. Crear");
+            Console.WriteLine("2. Obtener");
+            Console.WriteLine("3. Actualizar");
+            Console.WriteLine("4. Borrar");
+            Console.WriteLine("5. Regresar al menú principal");
 
-        Console.WriteLine("Presione una tecla para crear una fase");
-        Console.ReadKey();
-   
-        var createResponse = PhaseConsoleAppHandler.CreatePhaseAPI(channel, client);
+            Console.Write("Seleccione una opción: ");
 
-        Console.WriteLine("Presione una tecla para obtener todas las fases");
-        Console.ReadKey();
+            string input = Console.ReadLine();
+            int opcion = -1;
+            if (int.TryParse(input, out opcion))
+            {
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Crear fase");
+                        Console.WriteLine("Presione cualquier tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        Console.WriteLine("Obtener información de la fase");
+                        Console.WriteLine("Presione cualquier tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        Console.WriteLine("Actualizar información de la fase");
+                        Console.WriteLine("Presione cualquier tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                    case 4:
+                        Console.WriteLine("Eliminar información de la fase");
+                        Console.WriteLine("Presione cualquier tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                    case 5:
+                        return;
+                    default:
+                        Console.WriteLine("Opción inválida. Por favor, seleccione una opción válida.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Por favor, ingrese un número entero.");
+            }
 
-        PhaseConsoleAppHandler.GetAllPhasesAPI(channel, client, createResponse);
+            Console.WriteLine();
+            Console.WriteLine("Presione cualquier tecla para continuar...");
+            Console.ReadKey();
+        }
+    }
 
-        Console.WriteLine("Presione una tecla para modificar la fase");
-        Console.ReadKey();
+    static void SubMenuOperacion()
+    {
+        bool salirSubMenu = false;
+        while (!salirSubMenu)
+        {
+            Console.Clear();
+            Console.WriteLine("Menú Operacion");
+            Console.WriteLine("---------");
+            Console.WriteLine("1. Crear");
+            Console.WriteLine("2. Obtener");
+            Console.WriteLine("3. Actualizar");
+            Console.WriteLine("4. Borrar");
+            Console.WriteLine("5. Regresar al menú principal");
 
-       PhaseConsoleAppHandler.GetPhaseByIdAPI(channel, client, createResponse);
+            Console.Write("Seleccione una opción: ");
 
-        
-        var client1 = new gProtos.PhaseExecution.PhaseExecutionClient(channel);
+            string input = Console.ReadLine();
+            int opcion = -1;
+            if (int.TryParse(input, out opcion))
+            {
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Crear Operacion");
+                        Console.WriteLine("Presione cualquier tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        Console.WriteLine("Obtener información de la Operacion");
+                        Console.WriteLine("Presione cualquier tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        Console.WriteLine("Actualizar información de la operacion");
+                        Console.WriteLine("Presione cualquier tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                    case 4:
+                        Console.WriteLine("Eliminar información de la operacion");
+                        Console.WriteLine("Presione cualquier tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                    case 5:
+                        return;
+                    default:
+                        Console.WriteLine("Opción inválida. Por favor, seleccione una opción válida.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Por favor, ingrese un número entero.");
+            }
 
-        Console.WriteLine("Presione una tecla para crear la ejecucion de una fase");
-        Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine("Presione cualquier tecla para continuar...");
+            Console.ReadKey();
+        }
+    }
 
-        var createResponse1 = PhaseExecutionConsoleAppHandler.CreatePhaseExecutionAPI(channel, client1 );
+    static void SubMenuUnidadProcedimiento()
+    {
+        bool salirSubMenu = false;
+        while (!salirSubMenu)
+        {
+            Console.Clear();
+            Console.WriteLine("Menú Fase");
+            Console.WriteLine("---------");
+            Console.WriteLine("1. Crear");
+            Console.WriteLine("2. Obtener");
+            Console.WriteLine("3. Actualizar");
+            Console.WriteLine("4. Borrar");
+            Console.WriteLine("5. Regresar al menú principal");
 
-        Console.WriteLine("Presione una tecla para obtener todas las ejecuciones de fase");
-        Console.ReadKey();
+            Console.Write("Seleccione una opción: ");
 
-        PhaseExecutionConsoleAppHandler.GetAllPhaseExecutionAPI(channel, client1);
+            string input = Console.ReadLine();
+            int opcion = -1;
+            if (int.TryParse(input, out opcion))
+            {
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Crear unidad de procedimiento");
+                        Console.WriteLine("Presione cualquier tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        Console.WriteLine("Obtener información de la unidad de procedimiento");
+                        Console.WriteLine("Presione cualquier tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        Console.WriteLine("Actualizar información de la unidad de procedimiento");
+                        Console.WriteLine("Presione cualquier tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                    case 4:
+                        Console.WriteLine("Eliminar información de la unidad de procedimiento");
+                        Console.WriteLine("Presione cualquier tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                    case 5:
+                        return;
+                    default:
+                        Console.WriteLine("Opción inválida. Por favor, seleccione una opción válida.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Por favor, ingrese un número entero.");
+            }
 
-        Console.WriteLine($"Presione una tecla para obtener la fase con Id {createResponse1.ID}");
-        Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine("Presione cualquier tecla para continuar...");
+            Console.ReadKey();
+        }
+    }
 
-        PhaseExecutionConsoleAppHandler.GetPhaseExecutionByIdAPI(channel, client1, createResponse1 );
+    static void SubMenuEjecucionFase()
+    {
+        bool salirSubMenu = false;
+        while (!salirSubMenu)
+        {
+            Console.Clear();
+            Console.WriteLine("Menú Ejecucion de fase");
+            Console.WriteLine("---------");
+            Console.WriteLine("1. Crear");
+            Console.WriteLine("2. Obtener");
+            Console.WriteLine("3. Actualizar");
+            Console.WriteLine("4. Borrar");
+            Console.WriteLine("5. Regresar al menú principal");
 
-        Console.WriteLine("Presione una tecla para modificar la faseExecution");
-        Console.ReadKey();
+            Console.Write("Seleccione una opción: ");
 
-        PhaseExecutionConsoleAppHandler.UpdatePhaseExecutionAPI(channel, client1, createResponse1 );
+            string input = Console.ReadLine();
+            int opcion = -1;
+            if (int.TryParse(input, out opcion))
+            {
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Crear ejecucion de fase");
+                        Console.WriteLine("Presione cualquier tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        Console.WriteLine("Obtener información de la ejecucion de fase");
+                        Console.WriteLine("Presione cualquier tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        Console.WriteLine("Actualizar información de la ejecucion de fase");
+                        Console.WriteLine("Presione cualquier tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                    case 4:
+                        Console.WriteLine("Eliminar información de la ejecucion de fase");
+                        Console.WriteLine("Presione cualquier tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                    case 5:
+                        return;
+                    default:
+                        Console.WriteLine("Opción inválida. Por favor, seleccione una opción válida.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Por favor, ingrese un número entero.");
+            }
 
+            Console.WriteLine();
+            Console.WriteLine("Presione cualquier tecla para continuar...");
+            Console.ReadKey();
+        }
+    }
 
-        Console.WriteLine("Presione una tecla para eliminar la faseExecution");
-        Console.ReadKey();
+    static void SubMenuEjecucionOperacion()
+    {
+        bool salirSubMenu = false;
+        while (!salirSubMenu)
+        {
+            Console.Clear();
+            Console.WriteLine("Menú Ejecucion de operacion");
+            Console.WriteLine("---------");
+            Console.WriteLine("1. Crear");
+            Console.WriteLine("2. Obtener");
+            Console.WriteLine("3. Actualizar");
+            Console.WriteLine("4. Borrar");
+            Console.WriteLine("5. Regresar al menú principal");
 
-        PhaseExecutionConsoleAppHandler.DeletePhaseExecutionAPI(channel, client1, createResponse1);
+            Console.Write("Seleccione una opción: ");
 
-        Console.WriteLine("Presione una tecla para eliminar la fase");
-        Console.ReadKey();
+            string input = Console.ReadLine();
+            int opcion = -1;
+            if (int.TryParse(input, out opcion))
+            {
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Crear ejecucion de operacion");
+                        Console.WriteLine("Presione cualquier tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        Console.WriteLine("Obtener información de la ejecucion de operacion");
+                        Console.WriteLine("Presione cualquier tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        Console.WriteLine("Actualizar información de la ejecucion de operacion");
+                        Console.WriteLine("Presione cualquier tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                    case 4:
+                        Console.WriteLine("Eliminar información de la ejecucion de operacion");
+                        Console.WriteLine("Presione cualquier tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                    case 5:
+                        return;
+                    default:
+                        Console.WriteLine("Opción inválida. Por favor, seleccione una opción válida.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Por favor, ingrese un número entero.");
+            }
 
-        PhaseConsoleAppHandler.DeletePhaseAPI(channel, client, createResponse);
+            Console.WriteLine();
+            Console.WriteLine("Presione cualquier tecla para continuar...");
+            Console.ReadKey();
+        }
+    }
 
+    static void SubMenuEjecucionUnidadProcedimiento()
+    {
+        bool salirSubMenu = false;
+        while (!salirSubMenu)
+        {
+            Console.Clear();
+            Console.WriteLine("Menú Ejecucion de unidad de procedimiento");
+            Console.WriteLine("---------");
+            Console.WriteLine("1. Crear");
+            Console.WriteLine("2. Obtener");
+            Console.WriteLine("3. Actualizar");
+            Console.WriteLine("4. Borrar");
+            Console.WriteLine("5. Regresar al menú principal");
 
-        channel.Dispose();
+            Console.Write("Seleccione una opción: ");
 
+            string input = Console.ReadLine();
+            int opcion = -1;
+            if (int.TryParse(input, out opcion))
+            {
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Crear Ejecucion de unidad de procedimiento");
+                        Console.WriteLine("Presione cualquier tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        Console.WriteLine("Obtener información de la Ejecucion de unidad de procedimiento");
+                        Console.WriteLine("Presione cualquier tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        Console.WriteLine("Actualizar información de la Ejecuion de unidad de procedimiento");
+                        Console.WriteLine("Presione cualquier tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                    case 4:
+                        Console.WriteLine("Eliminar información de la unidad de procedimiento");
+                        Console.WriteLine("Presione cualquier tecla para continuar...");
+                        Console.ReadKey();
+                        break;
+                    case 5:
+                        return;
+                    default:
+                        Console.WriteLine("Opción inválida. Por favor, seleccione una opción válida.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Por favor, ingrese un número entero.");
+            }
 
+            Console.WriteLine();
+            Console.WriteLine("Presione cualquier tecla para continuar...");
+            Console.ReadKey();
+        }
     }
 }
+
+
